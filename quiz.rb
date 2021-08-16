4f45ccbe30510c430b1b938eea63ad77a6c9a5cb
Saim00786 <muhammad.saim@devsinc.com>
2021-08-16 14:19:38 +0500

    some more rubocop errors removed

--- quiz.rb
+++ quiz.rb
@@ -26,38 +26,38 @@ def start_quiz(questions,total_time=30)
     total_time.times do
       sleep(1)
     end
-    puts "Time out."
+    puts 'Time out.'
     t1.exit
   }
   t1 = Thread.new {
     questions.each{
       |q|
-      system("clear")
+      system('clear')
       sec = Time.now.to_i - before
       puts "You have #{total_time - sec} seconds to solve these Questions. :)"
-      puts "What is #{q["Q"]} ?"
+      puts "What is #{q['Q']} ?"
       while ans = Integer(gets.chomp) rescue true
         if ans.is_a? Integer
-          count = count+1 if ans.to_s == q["A"]
+          count = count+1 if ans.to_s == q['A']
           break
         else
-          puts "Invalid. Enter again"
+          puts 'Invalid. Enter again'
         end
       end
     }
     t2.exit
-    system("clear")
+    system('clear')
   }
   t1.join
   t2.join
   count
 end
 
-questions = get_Questions_and_Answers("problems.csv")
+questions = get_questions_and_answers('problems.csv')
 puts 'Enter time (leave empty for default):'
 timer = gets.strip.to_i
 
 system('clear')
 
-correct = startQuiz(questions,timer)
+correct = start_quiz(questions, timer)
 puts "#{correct} out of #{questions.length} questions were correct."

eec8e11a233f33c10b12ebc06592f17338d32b87
Saim00786 <muhammad.saim@devsinc.com>
2021-08-16 14:14:29 +0500

    applied rubocop suggestins

--- quiz.rb
+++ quiz.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 def get_questions_and_answers(filename)
-  fd = File.open(filename,"r")
-  str =  fd.read
-  lines  = str.split()
-  questions =[]
-  lines.each {|a|
-    tmp = Hash.new
-    tmp["Q"],tmp["A"] = a.split(',')
+  fd = File.open(filename, 'r')
+  str = fd.read
+  lines = str.split
+  questions = []
+  lines.each { |a|
+    tmp = {}
+    tmp['Q'], tmp['A'] = a.split(',')
     questions << tmp
   }
   questions
@@ -19,20 +19,16 @@ def start_quiz(questions,total_time=30)
   end
   puts 'Press enter to start the Quiz...'
   gets
-
   before = Time.now.to_i
   count = 0
   t1 = nil;
-
   t2 = Thread.new{
     total_time.times do
       sleep(1)
     end
-
     puts "Time out."
     t1.exit
   }
-
   t1 = Thread.new {
     questions.each{
       |q|
@@ -40,7 +36,6 @@ def start_quiz(questions,total_time=30)
       sec = Time.now.to_i - before
       puts "You have #{total_time - sec} seconds to solve these Questions. :)"
       puts "What is #{q["Q"]} ?"
-
       while ans = Integer(gets.chomp) rescue true
         if ans.is_a? Integer
           count = count+1 if ans.to_s == q["A"]
@@ -49,7 +44,6 @@ def start_quiz(questions,total_time=30)
           puts "Invalid. Enter again"
         end
       end
-
     }
     t2.exit
     system("clear")
@@ -59,12 +53,11 @@ def start_quiz(questions,total_time=30)
   count
 end
 
-questions= get_Questions_and_Answers("problems.csv")
-puts "Enter time (leave empty for default):"
+questions = get_Questions_and_Answers("problems.csv")
+puts 'Enter time (leave empty for default):'
 timer = gets.strip.to_i
 
-system("clear")
+system('clear')
 
 correct = startQuiz(questions,timer)
 puts "#{correct} out of #{questions.length} questions were correct."
-
